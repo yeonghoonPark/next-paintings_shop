fec82ffb1d5faf7f1bf420c6ae86538aea1f2b21
yeonghoon Park <huklmnj2@naver.com>
2023-06-09 20:28:56 +0900

    edit globals.scss

--- app/components/homepage/new-arrival/NewArrival.js
+++ app/components/homepage/new-arrival/NewArrival.js
@@ -4,12 +4,12 @@ import useSWR from "swr";
 import ItemCard from "../../base/ItemCard";
 
 export default function NewArrival() {
-  const { data, isLoading, error } = useSWR("/api/product/new_arrival/get");
+  const { data } = useSWR("/api/product/new_arrival/get");
   const newPaintings = data?.data;
 
   return (
     <section className='new-arrival'>
-      <h2>New Arrival</h2>
+      <h2 className='new-arrival-title'>New Arrival</h2>
       <div className='item-box'>
         {newPaintings && (
           <>
