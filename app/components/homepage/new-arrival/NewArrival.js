9c31bfe20797c6e2327909914db41092492ad1ad
yeonghoon Park <huklmnj2@naver.com>
2023-06-12 18:45:08 +0900

    added react-spinners

--- app/components/homepage/new-arrival/NewArrival.js
+++ app/components/homepage/new-arrival/NewArrival.js
@@ -2,15 +2,21 @@
 
 import useSWR from "swr";
 import ItemCard from "../../base/ItemCard";
+import MoonLoader from "react-spinners/MoonLoader";
 
 export default function NewArrival() {
-  const { data } = useSWR("/api/product/new_arrival/get");
+  const { data, isLoading } = useSWR("/api/product/new_arrival/get");
   const newPaintings = data?.data;
 
   return (
     <section className='new-arrival'>
       <h2 className='new-arrival-title'>New Arrival</h2>
       <div className='item-box'>
+        {isLoading && (
+          <div className='new-loading'>
+            <MoonLoader color='#f35129' />
+          </div>
+        )}
         {newPaintings && (
           <>
             {newPaintings.map((cV) => (
